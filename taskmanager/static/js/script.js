4ed6e7932da41f0744243fff076de285c718536d
Matt <mjd014@live.co.uk>
2022-09-09 18:43:16 +0000

    imported code from materialize to initialize our sidenav.

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+// Sidebar initialization from Materialize
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
