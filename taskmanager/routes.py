922afeaf2999b402031e5df28f348931b727c73b
Matt <mjd014@live.co.uk>
2022-09-10 19:26:50 +0000

    added if statement to 'add_category' route that adds a category created by the user.

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
+from taskmanager import Category, Task
 
 
 @app.route("/")
@@ -11,7 +12,14 @@ def home():
 def categories():
     return render_template("categories.html")
 
-
+# If statement checking if method from our form is 'POST'.
+# If so, it adds a category, created by user, adds to our database.
+# Redirects user back to 'categories.hmtl' when category is added.
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
+    if request.method == "POST":
+        category = Category
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
